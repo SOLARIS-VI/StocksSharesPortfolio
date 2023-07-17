f9dfaa68784e474a42aeb69ba0ed5fa518eaf210
ZuhayrK00 <zuhayrk00@gmail.com>
2023-07-17 16:04:00 +0100

    added symbols

--- client/src/components/FullListItem.js
+++ client/src/components/FullListItem.js
@@ -2,12 +2,12 @@
 import React from "react";
 
 const FullListItem = ({ share }) => {
-  const { name, symbol } = share;
+  const { name, ticker } = share;
 
   return (
     <li>
       <h3>{name}</h3>
-      <p>Symbol: {symbol}</p>
+      <p>Symbol: {ticker}</p>
     </li>
   );
 };
