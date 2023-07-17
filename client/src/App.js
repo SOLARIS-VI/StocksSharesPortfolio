bc2ba74ddca0374a146b2be21f9af201da2b27af
Steven <stevenjohnmorgan@gmail.com>
2023-07-17 19:09:58 +0100

    portfolio page updated

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,9 @@ function App() {
   const [symbol, setSymbol] = useState("APPL");
   const [stocks, setStocks] = useState([]);
   const [fullStocks, setFullStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
   const [candles, setCandles] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:9000/api/shares")
       .then((res) => res.json())
@@ -40,6 +42,7 @@ function App() {
         console.error("Error fetching stocks:", error);
       });
   }, []);
+
   useEffect(() => {
     ShareService.getStocks()
       .then((data) => {
@@ -50,11 +53,24 @@ function App() {
         console.error("Error fetching stocks:", error);
       });
   }, []);
+
+  useEffect(() => {
+    ShareService.getPortfolioStocks()
+      .then((data) => {
+        console.log(data);
+        setPortfolio(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching portfolio:", error);
+      });
+  }, []);
+
   const handleGetCandles = () => {
     const newCandles = api_auth.getStockCandles(symbol, "D", timeFrom, timeNow);
     setCandles(newCandles);
-    console.log(candles)
-  }
+    console.log(candles);
+  };
+
   return (
     <Router>
       <Navbar />
@@ -64,12 +80,25 @@ function App() {
             <Route
               path="/sharedetails/:id"
               element={
-                <ShareDetails setTimeFrom={setTimeFrom} timeNow={timeNow} handleGetCandles={handleGetCandles} />
+                <ShareDetails
+                  setTimeFrom={setTimeFrom}
+                  timeNow={timeNow}
+                  handleGetCandles={handleGetCandles}
+                />
               }
             />
             <Route path="/" element={<ShareList stocks={stocks} />} />
-            <Route path="/portfolio" element={<PortfolioList />} />
-            <Route path="/fullList" element={<FullList fullStocks={fullStocks} stocks={stocks} setStocks={setStocks} />} />
+            <Route path="/portfolio" element={<PortfolioList portfolio={portfolio} />} />
+            <Route
+              path="/fullList"
+              element={
+                <FullList
+                  fullStocks={fullStocks}
+                  stocks={stocks}
+                  setStocks={setStocks}
+                />
+              }
+            />
           </Routes>
         </ContentContainer>
       </AppContainer>
--- client/src/components/PortfolioList.js
+++ client/src/components/PortfolioList.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import ShareItem from "./ShareItem";
 
-const Portfolio = () => {
+const PortfolioList = ({ portfolio }) => {
+  const portfolioNodes = portfolio.map((portfolioItem) => (
+    <ShareItem key={portfolioItem.symbol} share={portfolioItem} />
+  ));
+
   return (
-    <h1>Portfolio Page</h1>
-  )
-}
+    <>
+      <h1>Portfolio Page</h1>
+      <ul>{portfolioNodes}</ul>
+    </>
+  );
+};
 
-export default Portfolio
+export default PortfolioList;
