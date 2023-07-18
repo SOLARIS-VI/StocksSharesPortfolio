0d270859d24dd42a6f444d331892aefdf079358d
ZuhayrK00 <zuhayrk00@gmail.com>
2023-07-18 14:32:30 +0100

    added padding to details

--- client/src/components/PortfolioList.js
+++ client/src/components/PortfolioList.js
@@ -4,18 +4,18 @@ import ShareItem from "./ShareItem";
 const PortfolioList = ({ portfolio }) => {
   const portfolioNodes = portfolio.map((portfolioItem) => (
     <ShareItem
-      key={portfolioItem.id}
-      id={portfolioItem.id}
-      name={portfolioItem.name}
-      symbol={portfolioItem.symbol}
-      shares={portfolioItem.shares}
+      // key={portfolioItem.id}
+      // id={portfolioItem.id}
+      // name={portfolioItem.name}
+      // symbol={portfolioItem.symbol}
+      // shares={portfolioItem.shares}
     />
   ));
 
   return (
     <>
       <h1>Portfolio Page</h1>
-      <ul>{portfolioNodes}</ul>
+      {/* <ul>{portfolioNodes}</ul> */}
     </>
   );
 };
